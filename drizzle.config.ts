11c6929587d7e5b3073fd1668d805a7553efd27e
Björn <bjoern.enders@googlemail.com>
2024-10-20 18:57:03 +0200

    fix ca reading

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from "drizzle-kit";
-import { readFileSync } from "fs";
 
 const POSTGRES_DB = process.env.POSTGRES_DB ?? "";
 const POSTGRES_USER = process.env.POSTGRES_USER ?? "";
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD ?? "";
 const POSTGRES_HOST = process.env.POSTGRES_HOST ?? "";
 const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT ?? "5432");
+const POSTGRES_CA = process.env.POSTGRES_CA ?? "";
+
+if (!POSTGRES_CA) {
+  throw new Error("POSTGRES_CA is not set");
+}
+
+const PREFIX = "base_";
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/lib/db/db-schema.ts",
   out: "./drizzle-sql",
-  tablesFilter: ["base_*"],
+  tablesFilter: PREFIX + "*",
   migrations: {
-    table: "base_migrations",
+    table: `${PREFIX}migrations`,
   },
   dbCredentials: {
     user: POSTGRES_USER,
@@ -23,7 +29,7 @@ export default defineConfig({
     database: POSTGRES_DB,
     ssl: {
       rejectUnauthorized: false,
-      ca: readFileSync("ca.pem").toString(),
+      ca: POSTGRES_CA,
     },
   },
 });
