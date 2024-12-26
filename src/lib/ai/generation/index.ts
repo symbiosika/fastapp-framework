8c6afa16473918ef590e4fc5e4da8c438580989d
Björn <bjoern.enders@googlemail.com>
2024-12-26 20:56:10 +0100

    fix

--- src/lib/ai/generation/index.ts
+++ src/lib/ai/generation/index.ts
@@ -250,8 +250,7 @@ export const templateChat = new TemplateChat({
 }}
   {{#role=assistant}}
     You are a helpful assistant and will help the user with his questions.
-    You will answer short and to the point. You will answer to everything.
-    Your answer will be in the language of the user.
+    You will answer to everything. Your answer will be in the language of the user.
   {{/role}}
 
   {{#role=user}}
