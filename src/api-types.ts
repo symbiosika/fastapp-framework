0f1b82cac0cb1ce7767d597fc3ed93206fae224d
Björn <bjoern.enders@googlemail.com>
2025-02-06 16:07:50 +0100

    adds types to sub package

---
+++ src/api-types.ts
@@ -0,0 +1,12 @@
+export type {
+  OrganisationsSelect,
+  OrganisationsInsert,
+  UsersSelect,
+  UsersInsert,
+  TeamsSelect,
+  TeamsInsert,
+  TeamMembersSelect,
+  TeamMembersInsert,
+  OrganisationInvitationsSelect,
+  OrganisationInvitationsInsert,
+} from "./lib/db/schema/users";
