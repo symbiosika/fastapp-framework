9f172cdb3fb781d41dd2cf1bdad795eabbf3a8df
Bjoern Enders <enders@symbiosika.de>
2025-04-15 17:29:58 +0200

    update

--- src/middleware-service.ts
+++ src/middleware-service.ts
@@ -2,16 +2,25 @@
  * Exposed the hono middlewares to check user permissions and authentication
  */
 
+import { validateScope } from "./lib/auth/available-scopes";
 import {
   authAndSetUsersInfo,
   authOrRedirectToLogin,
   checkUserPermission,
 } from "./lib/utils/hono-middlewares";
 
+import {
+  isOrganisationMember,
+  isOrganisationAdmin,
+} from "./routes/organisation";
+
 const middlewareService = {
   authAndSetUsersInfo,
   authOrRedirectToLogin,
   checkUserPermission,
+  isOrganisationMember,
+  isOrganisationAdmin,
+  validateScope,
 };
 
 export default middlewareService;
